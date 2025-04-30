b8325037d6cc825071ea566484f8b0a5fc200d15
antegral <antegral@antegral.net>
2025-05-01 02:40:47 +0900

    feat: Rename BufTimestamp2AsTime to PbTimestamp2AsTimePtr for consistency

--- util/protobuf.go
+++ util/protobuf.go
@@ -6,11 +6,19 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func BufTimestamp2AsTime(t *timestamppb.Timestamp) *time.Time {
+func PbTimestamp2AsTimePtr(t *timestamppb.Timestamp) *time.Time {
 	if t == nil {
 		return nil
 	}
 
 	tm := t.AsTime()
 	return &tm
+}
+
+func AsTimePtrToPbTimestamp(t *time.Time) *timestamppb.Timestamp {
+	if t == nil {
+		return nil
+	}
+
+	return timestamppb.New(*t)
 }
